25139479b06bf5afcae3245e6166c7fc0b918194
Jan Hofrichter <hofrrichter.jan1@gmail.com>
2022-06-23 12:39:03 +0200

    update

--- base/serializers.py
+++ base/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from base.models import item
 
-class ItemSerializer(serializers.ModelSerializer):
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = item
         fields = '__all__'
--- base/views.py
+++ base/views.py
@@ -1,3 +1,30 @@
+import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+# from base.models import item
+from .serializers import ItemSerializer
+
+
+@api_view(['GET'])
+def getData(object_name):
+    dict = []
+    with open("test_data.json", "r") as file:
+        data_input = json.load(file)
+    #for x in data_input:
+    #    if object_name in x.keys():
+    #        dict.add(x)
+    #items = item.objects.all()
+    #serializer = ItemSerializer(items, many=True)
+    #return JsonResponse(data_input,safe=False)
+    return Response(data_input)
+
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
