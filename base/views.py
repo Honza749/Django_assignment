34c8c8a5fe1f2e588585d2362ac093f159d85b06
Honza749 <55848643+Honza749@users.noreply.github.com>
2022-06-24 23:16:06 +0200

    Update views.py
    Adding comments

--- base/views.py
+++ base/views.py
@@ -6,18 +6,37 @@ from rest_framework.response import Response
 
 with open("test_data.json", "r") as file:
     data_input = json.load(file)
-
 data: list = data_input
+
+    
+def Merge(dict_1, dict_2):
+    """Merge 2 dictionaries"""
+
+    result = dict_1 | dict_2
+    return result
+
+
+def is_valid_json(input_json):
+    """Control, if data format is json"""
+
+    try:
+        json.loads(input_json)
+    except ValueError as e:
+        return False
+    return True
 
 
 @api_view(["GET"])
 def get_all_data(request):
     """Returns all object available"""
+    
     return Response(data)
 
 
 @api_view(["GET"])
 def get_by_name_id(request, model_name, object_id):
+    """Returns object with specific name and id"""
+ 
     for x in data:
         if model_name in x.keys():
             if x[model_name]["id"] == int(object_id):
@@ -26,6 +45,8 @@ def get_by_name_id(request, model_name, object_id):
 
 @api_view(["GET"])
 def get_by_name(request, model_name):
+    """Return all objects with specific name"""
+
     output = []
     for x in data:
         if model_name in x.keys():
@@ -33,21 +54,10 @@ def get_by_name(request, model_name):
     return Response(output)
 
 
-def Merge(dict_1, dict_2):
-    result = dict_1 | dict_2
-    return result
-
-
-def is_valid_json(input_json):
-    try:
-        json.loads(input_json)
-    except ValueError as e:
-        return False
-    return True
-
-
 @api_view(["POST"])
 def process_import(request):
+    """Import data"""
+
     body_unicode = request.body.decode("utf-8")
     if not is_valid_json(body_unicode):
         raise SuspiciousOperation("Invalid request; wrong data posted!")
